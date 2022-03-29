6e5670699164ef3436e2c438e926c5985a730389
rauld2686 <rauld2686gmail.com>
2022-03-28 19:22:13 -0500

    navbar

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,15 @@
+import './NavBar.css'
+const NavBar = () => {
+    return(
+        <nav>
+            <h1>Bally Pets</h1>
+            <ul className="links_navbar">
+                <li><a href="#">ALIMENTOS</a></li>
+                <li><a href="#">HIGIENE</a></li>
+                <li><a href="#">JUGUETES</a></li>
+            </ul>
+        </nav>
+    )
+}
+
+export default NavBar
